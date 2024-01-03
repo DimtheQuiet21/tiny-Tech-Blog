de55f7a4e6fb3293a9e66f541a5c7e15ee4e5d84
Alan Lee <aflee@wisc.edu>
2024-01-03 09:49:17 -0600

    Adding Configuration to Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,22 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'mysql',
-    port: 3306,
-  }
-);
+    logging: true, // Disable logging for production (you can enable it for debugging)
+  });
+} else {
+  const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+};
 
 module.exports = sequelize;
